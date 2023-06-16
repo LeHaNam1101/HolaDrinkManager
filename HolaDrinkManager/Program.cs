26af9601f0365871498332341ed5eebab0dd0f18
LeHaNam1101 <clonedz11@gmail.com>
2023-06-17 02:01:34 +0700

    up

--- HolaDrinkManager/Program.cs
+++ HolaDrinkManager/Program.cs
@@ -8,6 +8,7 @@ namespace HolaDrinkManager
         [STAThread]
         static void Main()
         {
+            //TestLeHaNam
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
